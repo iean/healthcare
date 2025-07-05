6b1f73e4e6e4bbe91ce44decee949d9f6b475fb8
Masud Alam <masud.official@gmail.com>
2025-07-05 23:53:00 +0600

    Add simple page navigation

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import "../styles/style.scss";
 import config from "@config/config.json";
 import theme from "@config/theme.json";
-import Header from "@layouts/partials/Header";
+import SimpleHeader from "@layouts/partials/SimpleHeader";
 import Footer from "@layouts/partials/Footer";
 import Providers from "@layouts/partials/Providers";
 import { Inter, Merriweather, Playfair_Display } from "next/font/google";
@@ -40,7 +40,7 @@ export default function RootLayout({ children }) {
       className={`${inter.variable} ${playfair.variable} ${merriweather.variable}`}
     >
       <body className="font-sans text-brandText antialiased">
-        <Header />
+        <SimpleHeader />
         <Providers>{children}</Providers>
         <Footer />
       </body>
