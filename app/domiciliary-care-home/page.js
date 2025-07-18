0707609b2a8bd499f58dc9f761e299e4389663bc
masud <masud.official@gmail.com>
2025-07-18 11:55:53 +0600

    (Fix) build issue

--- app/domiciliary-care-home/page.js
+++ app/domiciliary-care-home/page.js
@@ -2,7 +2,7 @@ import config from "@config/config.json";
 import SeoMeta from "@layouts/SeoMeta";
 import Services from "@layouts/partials/Services";
 import Contact from "@layouts/Contact";
-import { getListPage, getRegularPage } from "../lib/contentParser";
+import { getListPage, getRegularPage } from "../../lib/contentParser";
 
 const DomiciliaryCareHome = async () => {
   const homePage = await getListPage("content/_index.md");
