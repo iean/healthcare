6e98e8f5b2047fa1e440b5b37eb79562ccc54bb2
Masud Alam <masud.official@gmail.com>
2025-07-19 12:26:03 +0600

    Add Our Careers page for domiciliary

---
+++ app/domiciliary/our-careers/page.js
@@ -0,0 +1,13 @@
+import CareersBanner from "@layouts/domiciliary/Careers/Banner";
+import Intro from "@layouts/domiciliary/Careers/Intro";
+import Benefits from "@layouts/domiciliary/Careers/Benefits";
+
+const OurCareers = () => (
+  <>
+    <CareersBanner />
+    <Intro />
+    <Benefits />
+  </>
+);
+
+export default OurCareers;
